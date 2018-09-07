5cb4b5ad40aa3dab01c2562a502c232eb03ac00b
root <root@ip-172-20-0-42.ec2.internal>
2018-09-07 15:42:53 +0000

    modified:   bin/deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #
-# only run this in a production environment
+# only run this in a production environmei
+
 if [[ "$RAILS_ENV" = "production" ]]; then
   cd /data/cpease_todo_mushyyGH/current
-  ey_bundler_binstubs/ey deploy -a EY-Dedicated-Internal -e pillpack_testing -m --serverside-version=2.6.14
+  ey_bundler_binstubs/ey deploy -a cpease_todo_mushyyGH -e pillpack_testing -m --serverside-version=2.6.14 --ref HEAD
 fi
+
