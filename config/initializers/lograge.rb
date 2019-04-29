d0c75cc919d78d3e47a6a552bef41d0b5512105d
Callum Pease <cpease1997@hotmail.com>
2019-04-29 20:22:18 +0100

    lograge issues fix

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,7 +1,4 @@
 Rails.application.configure do
+        config.lograge.keep_original_rails_log = true
 	config.lograge.enabled = true
-	config.lograge.formatter = Lograge::Formatters:Logstash.new
-	config.lograge.custom_options = lambda do |event|
-		event.payload
-	end
 end
